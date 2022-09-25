b96e52f9ecb0c6fd61342ac83d0391c0a16a6ad9
Umut <57041978+Umukka@users.noreply.github.com>
2022-09-25 20:55:17 +0300

    Update siyamaglari.py

--- siyamaglari.py
+++ siyamaglari.py
@@ -1,18 +1,15 @@
-from tensorflow.keras.applications import Xception
-from tensorflow.keras.applications import VGG19
-from tensorflow.keras import Model
-from tensorflow.keras import Sequential
-from tensorflow.keras import Input
+from tensorflow.keras.applications import Xception, VGG19
+from tensorflow.keras import Model, Input, Sequential
 from tensorflow.keras.layers import *
+
 from tensorflow import expand_dims
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import tensorflow as tf
 
-import skimage
-
-import random
-
 import numpy as np
+import cv2 as cv
+import skimage
+import random
 
 pretrained_model = Xception()
 pretrained_model.trainable = False
@@ -23,11 +20,11 @@ layers = []
 for layer in pretrained_model.layers:
   layers.append(layer)
 ind = -2
-extractor_layer = {"name":layers[ind].name, "index":ind}
+extractor_layer = {"name": layers[ind].name, "index": ind}
 
 print("Xception:")
 print("input shape: "+str(input_shape)) 
-print("extractor layer: name={}, index={}".format(extractor_layer["name"], extractor_layer["index"]))
+print(f"extractor layer: name={extractor_layer["name"]}, index={extractor_layer["index"]}")
 
 """
 pretrained_model = VGG19()
@@ -54,19 +51,18 @@ print("extractor model:")
 print("input shape: "+str(extractor_model.input_shape))
 print("output shape: "+str(extractor_model.output_shape))
 
-import cv2 as cv
 normalize = tf.keras.Sequential([
                                 tf.keras.layers.experimental.preprocessing.Resizing(299,299),
                                 tf.keras.layers.experimental.preprocessing.Rescaling(1./255),
 ])
 
-posimg = cv.imread("/content/img1.jpg")
+pos_img = cv.imread("/content/img1.jpg")
 
-posimg = tf.expand_dims(normalize(posimg), 0)
+pos_img = tf.expand_dims(normalize(pos_img), 0)
 posf = extractor_model.predict(posimg)[0]
 
-negimg = cv.imread("/content/img2.jpg")
+neg_img = cv.imread("/content/img2.jpg")
 
-negimg = tf.expand_dims(normalize(negimg), 0)
-negf = extractor_model.predict(negimg)[0]
+neg_img = tf.expand_dims(normalize(neg_img), 0)
+negf = extractor_model.predict(neg_img)[0]
 abs(posf-negf).sum()/2048
